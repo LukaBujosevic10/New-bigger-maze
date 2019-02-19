c7839d279f1040c5e01c21f78e42b5a155dd01a8
LukaBujosevic10 <lukabujosevic@gmail.com>
2019-02-19 17:22:28 +0100

    w

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
     ctx.fillStyle = "yellow";
     ctx.textAlign = "center";
     ctx.rotate(90*Math.PI/180);
-    ctx.fillText("MEGA MAZE v146", 200, -150);
+    ctx.fillText("MEGA MAZE v147", 200, -150);
     ctx.font = "30px Comic Sans MS";
     ctx.fillText("Loading Level...", 200, -100);
     ctx.fillStyle = "white";
@@ -127,7 +127,7 @@ function promena_pozicije(smer) {
       }
 
     }*/
-      if (top < 960-450 && top > 250) {
+      if (top < 960-450 && player.position.y > 250) {
         if (960-450-top > v_gd) {
           $(canvas).css('top', '-='+v_gd)
         }else {
